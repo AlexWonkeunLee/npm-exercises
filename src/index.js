eaa95dc3b4dbdde28d1cc1231f8e5a26cfbc9bcd
AlexWonkeunLee <AlexWonkeunLee@gmail.com>
2019-11-15 11:27:31 -0600

    npm and module exercise

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import $ from "jquery";
+import hello from "./say-hello";
 const sayHello = () => console.log("Hello");
 sayHello();
 $(document).ready(function(){
     $('body').html("Jquery still working");
+    console.log(hello);
 });
---
+++ src/say-hello.js
@@ -0,0 +1,5 @@
+const anotherHello = () => {
+    return "Hello, how are you doing?";
+};
+
+module.exports = anotherHello();
